ba9f2375e4dd12a04388766202c2a074e57750ac
Guillaume Tuttobene <gtuttobene@mdweb.fr>
2025-06-04 15:11:31 +0900

    fix glow effect

--- src/client/java/com/tomokisan/advancedmana/AdvancedManaClient.java
+++ src/client/java/com/tomokisan/advancedmana/AdvancedManaClient.java
@@ -1,16 +1,11 @@
 package com.tomokisan.advancedmana;
 
-import com.tomokisan.advancedmana.client.render.ManaDetectorBlockEntityRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 
 public class AdvancedManaClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// Enregistrer le renderer pour le détecteur de mana
-		BlockEntityRendererRegistry.register(
-			AdvancedMana.MANA_DETECTOR_BLOCK_ENTITY, 
-			ManaDetectorBlockEntityRenderer::new
-		);
+		// Le rendu se fait maintenant via les blockstates et textures animées
+		// Plus besoin de renderer personnalisé !
 	}
 }
